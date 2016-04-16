93c67777d05f9a1dab5b146cad5af9e5afc016b7
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-04-17 01:02:57 +0900

    moved from PCL.jl

--- src/PCLFilters.jl
+++ src/PCLFilters.jl
@@ -1,5 +1,98 @@
 module PCLFilters
 
-# package code goes here
+export AbstractFilter, AbstractVoxelGridFilter,
+    UniformSampling, PassThrough, VoxelGrid, ApproximateVoxelGrid,
+    StatisticalOutlierRemoval, RadiusOutlierRemoval, ExtractIndices,
+    setRadiusSearch, setFilterFieldName, setFilterLimits, setLeafSize,
+    setKeepOrganized, setMeanK, setStddevMulThresh, setMinNeighborsInRadius,
+    setNegative
+
+using LibPCL
+using PCLCommon
+using Cxx
+
+libpcl_filters = LibPCL.find_library_e("libpcl_filters")
+try
+    Libdl.dlopen(libpcl_filters, Libdl.RTLD_GLOBAL)
+catch e
+    warn("You might need to set DYLD_LIBRARY_PATH to load dependencies proeprty.")
+    rethrow(e)
+end
+
+cxx"""
+#include <pcl/filters/uniform_sampling.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/approximate_voxel_grid.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/radius_outlier_removal.h>
+#include <pcl/filters/extract_indices.h>
+"""
+
+import Base: filter
+import PCLCommon: setInputCloud, setIndices
+
+abstract AbstractFilter <: PCLBase
+abstract AbstractVoxelGridFilter <: AbstractFilter
+
+setInputCloud(f::AbstractFilter, cloud::PointCloud) =
+    icxx"$(f.handle)->setInputCloud($(cloud.handle));"
+filter(f::AbstractFilter, cloud::cxxt"boost::shared_ptr<std::vector<int>>") =
+    icxx"$(f.handle)->filter(*$(cloud));"
+filter(f::AbstractFilter, cloud::PointCloud) =
+    icxx"$(f.handle)->filter(*$(cloud.handle));"
+
+for (name, supername) in [
+    (:UniformSampling, AbstractFilter),
+    (:PassThrough, AbstractFilter),
+    (:VoxelGrid, AbstractVoxelGridFilter),
+    (:ApproximateVoxelGrid, AbstractVoxelGridFilter),
+    (:StatisticalOutlierRemoval, AbstractFilter),
+    (:RadiusOutlierRemoval, AbstractFilter),
+    (:ExtractIndices, AbstractFilter),
+    ]
+    cxxname = "pcl::$name"
+    valname = symbol(name, "Val")
+    @eval begin
+        @defpcltype $name{T} <: $supername $cxxname
+        @defptrconstructor $name{T}() $cxxname
+        @defconstructor $valname{T}() $cxxname
+    end
+end
+
+setRadiusSearch(us::UniformSampling, ss) =
+    icxx"$(us.handle)->setRadiusSearch($ss);"
+
+setFilterFieldName(pass::PassThrough, name::AbstractString) =
+    icxx"$(pass.handle)->setFilterFieldName($(pointer(name)));"
+setFilterLimits(pass::PassThrough, lo, hi) =
+    icxx"$(pass.handle)->setFilterLimits($lo, $hi);"
+
+for f in [:setKeepOrganized, :setFilterLimitsNegative]
+    body = Expr(:macrocall, symbol("@icxx_str"), "\$(pass.handle)->$f(\$v);")
+    @eval $f(pass::PassThrough, v::Bool) = $body
+end
+
+setLeafSize(v::AbstractVoxelGridFilter, lx, ly, lz) =
+    icxx"$(v.handle)->setLeafSize($lx, $ly, $lz);"
+
+
+for f in [:setMeanK, :setStddevMulThresh]
+    body = Expr(:macrocall, symbol("@icxx_str"), "\$(s.handle)->$f(\$v);")
+    @eval $f(s::StatisticalOutlierRemoval, v) = $body
+end
+
+for f in [:setRadiusSearch, :setMinNeighborsInRadius]
+    body = Expr(:macrocall, symbol("@icxx_str"), "\$(r.handle)->$f(\$v);")
+    @eval $f(r::RadiusOutlierRemoval, v) = $body
+end
+
+for f in [:setNegative, :setIndices]
+    body = Expr(:macrocall, symbol("@icxx_str"), "\$(ex.handle)->$f(\$v);")
+    @eval $f(ex::ExtractIndices, v) = $body
+end
+
+setIndices(ex::ExtractIndices, indices::PointIndices) =
+    setIndices(ex, indices.handle)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,42 @@
+using PCLCommon
+using PCLIO
 using PCLFilters
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+table_scene_lms400_path = joinpath(dirname(@__FILE__), "data",
+    "table_scene_lms400.pcd")
+
+@testset "pcl::filters" begin
+    cloud = PointCloud{PointXYZ}(table_scene_lms400_path)
+    cloud_filtered = PointCloud{PointXYZ}()
+
+    uniform_sampling = UniformSampling{PointXYZ}()
+    setInputCloud(uniform_sampling, cloud)
+    setRadiusSearch(uniform_sampling, 0.01)
+    filter(uniform_sampling, cloud_filtered)
+    @test length(cloud_filtered) < length(cloud)
+
+    for sor in [
+        VoxelGrid{PointXYZ}(),
+        ApproximateVoxelGrid{PointXYZ}()
+        ]
+        setInputCloud(sor, cloud)
+        setLeafSize(sor, 0.01, 0.01, 0.01)
+        filter(sor, cloud_filtered)
+        @test length(cloud_filtered) < length(cloud)
+    end
+
+    sor = StatisticalOutlierRemoval{PointXYZ}()
+    setInputCloud(sor, cloud)
+    setMeanK(sor, 10)
+    setStddevMulThresh(sor, 1.0)
+    filter(sor, cloud_filtered)
+    @test length(cloud_filtered) < length(cloud)
+
+    r = RadiusOutlierRemoval{PointXYZ}()
+    setInputCloud(r, cloud)
+    setRadiusSearch(r, 0.2)
+    setMinNeighborsInRadius(r, 5)
+    filter(r, cloud_filtered)
+    @test length(cloud_filtered) < length(cloud)
+end
